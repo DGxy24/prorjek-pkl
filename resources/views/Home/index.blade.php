ae0369e03327437d923577994b8256e35ed85146
DGxy24 <dolyboantua@gmail.com>
2024-08-01 13:52:36 +0700

    percobaan

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -10,15 +10,29 @@
 
     <title>Hello, world!</title>
   </head>
-  <body>
-    <div>
-        <div class="fs-1">Selamat Data Di Aplikasi A</div>
-        <button type="button" class="btn btn-primary" >Login</button> <br>
-        <button type="button" class="btn btn-success">Sign Up</button>
+  <body bgcolor="aqua">
+
+    <div class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+      <h1 class="display-5 fw-bold">Selamat Datang di Aplikasi A</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Penjelasan singkat tentang aplikasi</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <a href=""class="btn btn-primary btn-lg px-4 gap-3">Login</a> 
+          <a href="" class="btn btn-success btn-lg px-4">Sign Up</a>
+        </div>
+      </div>
+    </div>
+    {{-- <div class="position-relative">
+
+        <div class="fs-1 position-absolute top-0 start-50 translate-middle">Selamat Data Di Aplikasi A</div>
+        <a href=""  class="btn btn-primary mb-2">Login</a><br>
+        <a href=""  class="btn btn-success">Sign Up</a><br>
+
 
     
     
-    </div>
+    </div> --}}
 
   
   </body>
