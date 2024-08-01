0df84c76af0ddfe80fef926e30cd50c9fca44ec4
DGxy24 <dolyboantua@gmail.com>
2024-08-01 15:00:36 +0700

    tes

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -7,22 +7,25 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="css/style.css">
     <title>Hello, world!</title>
   </head>
-  <body bgcolor="aqua">
+  <body class="color">
+  
+    <div class="px-4 py-5 my-5 text-center"style="color:#ffffff">
+      <img class="d-block mx-auto mb-4" src="img/logo.png" alt="" width="120" height="180">
+      <h1 class="display-5 fw-bold">SELAMAT DATANG DI APLIKASI
+        <br> PENGELOLAAN KELUHAN IT
+        <br>PENGADILAN NEGERI MEDAN</h1>
+      <div class="col-lg-6 mx-auto mt-5">
 
-    <div class="px-4 py-5 my-5 text-center">
-      <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-      <h1 class="display-5 fw-bold">Selamat Datang di Aplikasi A</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Penjelasan singkat tentang aplikasi</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <a href=""class="btn btn-primary btn-lg px-4 gap-3">Login</a> 
-          <a href="" class="btn btn-success btn-lg px-4">Sign Up</a>
+          <a href=""class="btn  btn-lg px-4 gap-3 me-3 " style="background:#324851; color:white">LOGIN</a> 
+          <a href="" class="btn  btn-lg px-4 "style="background:#324851; color:white">SIGN UP</a>
         </div>
       </div>
     </div>
+
     {{-- <div class="position-relative">
 
         <div class="fs-1 position-absolute top-0 start-50 translate-middle">Selamat Data Di Aplikasi A</div>
