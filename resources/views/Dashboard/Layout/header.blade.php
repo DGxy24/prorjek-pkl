7a08033e6a70a723720b9aaaf40c4f9a82a21bef
lindonbaynesjs <lindonsihombing05@gmail.com>
2024-08-07 14:26:35 +0700

    membuat profile

--- resources/views/Dashboard/Layout/header.blade.php
+++ resources/views/Dashboard/Layout/header.blade.php
@@ -1,23 +1,19 @@
 {{-- membuat header --}}
 
-<header class="navbar navbar-dark sticky-top  p-4 shadow" style="background-color: #7DA3A1">
-    <a class=" col-md-3 col-lg-2 me-0 px-3" href="/dashboard">
-        <img src="/img/logo.png" alt="" width="25%">
+<header class="navbar navbar-dark sticky-top  p-3 shadow" style="background-color: #7DA3A1">
+    <a class="" href="/dashboard">
+        <img src="/img/logo.png" alt="" width="40px" height="60px">
     </a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+
     {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
 
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
-            <form action="/logout" method="POST">
-                @csrf
-                <button type="submit" class="nav-link px-3 bg-dark border-0"> Logout
-                    <span data-feather="log-out"></span>
-                </button>
-            </form>
+            <a href="#" type="submit" class="nav-link px-3 border-none">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 me-2">User</span>
+                <img src="/img/profile-user.png" alt="" width="30px" height="30px">
+            </a>
         </div>
     </div>
+
 </header>
