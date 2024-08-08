9eff0448dbd71683c7363ef52beea229a1d4c2cc
lindonbaynesjs <lindonsihombing05@gmail.com>
2024-08-08 11:41:30 +0700

    menambah tabel menu tiket proses & selesai

--- resources/views/Dashboard/Tiket-selesai/index.blade.php
+++ resources/views/Dashboard/Tiket-selesai/index.blade.php
@@ -1,50 +1,88 @@
+{{-- membuat menu tabel tiket dan modal tiket selesai --}}
+
 @extends('dashboard.layout.main')
 
 @section('container')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Tiket Selesai</h1>
-
     </div>
 
-
-
-    {{-- <div class="table-responsive col-lg-8">
-        <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
+    <div class="table-responsive table-bordered w-100">
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Category</th>
+                    <th scope="col">ID Tiket</th>
+                    <th scope="col">Nama Bagian</th>
+                    <th scope="col">Permasalah</th>
+                    <th scope="col">Tindakan</th>
+                    <th scope="col">Bukin</th>
                     <th scope="col">Action</th>
-
                 </tr>
             </thead>
             <tbody>
-                @foreach ($posts as $post)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->category->name }}</td>
-                        <td>
-                            <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span
-                                    data-feather="eye"></span></a>
-
-                            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
-
-                            <form class="d-inline" action="/dashboard/posts/{{ $post->slug }}" method="POST">
-                                @method('delete')
-                                @csrf
-                                <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"> <span
-                                        data-feather="x-circle"></button>
-
-                            </form>
-
-                        </td>
-
-                    </tr>
-                @endforeach
+                <tr>
+                    <td>1</td>
+                    <td>1234</td>
+                    <td>IT</td>
+                    <td>System Down</td>
+                    <td>Restarted server</td>
+                    <td>pdf</td>
+                    <td>
+                        <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                            onclick="showTicketSelesai('1234', 'IT', 'System Down', 'Restarted server', 'pdf')">
+                            <span data-feather="eye">Lihat</span>
+                        </button>
+                        {{-- <a href="#" class="badge bg-warning"><span data-feather="edit">Edit</span></a>
+                        <form class="d-inline" action="" method="POST">
+                            @csrf
+                            <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')">Delete<span
+                                    data-feather="x-circle"></span></button>
+                        </form> --}}
+                    </td>
+                </tr>
             </tbody>
         </table>
-    </div> --}}
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ticket Selesai</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-sm">
+                        <tr>
+                            <th>ID Tiket</th>
+                            <td id="modalTicketId"></td>
+                        </tr>
+                        <tr>
+                            <th>Nama Bagian</th>
+                            <td id="modalNamaBagian"></td>
+                        </tr>
+                        <tr>
+                            <th>Permasalah</th>
+                            <td id="modalPermasalah"></td>
+                        </tr>
+                        <tr>
+                            <th>Tindakan</th>
+                            <td id="modalTindakan"></td>
+                        </tr>
+                        <tr>
+                            <th>Bukti</th>
+                            <td id="modalBukti"></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Selesai</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
