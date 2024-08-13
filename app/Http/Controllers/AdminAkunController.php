286ebab70e44a9159e049f15fe0ef8987e939700
lindonbaynesjs <lindonsihombing05@gmail.com>
2024-08-13 11:56:23 +0700

    simpan

--- app/Http/Controllers/AdminAkunController.php
+++ app/Http/Controllers/AdminAkunController.php
@@ -12,7 +12,7 @@ class AdminAkunController extends Controller
      */
     public function index()
     {
-        return view('Dashboard-admin.Users.index',[
+        return view('Dashboard-admin.Users.index', [
             'akun' => User::all()
         ]);
     }
@@ -20,10 +20,7 @@ class AdminAkunController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-      
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
