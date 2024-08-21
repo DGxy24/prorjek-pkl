0240534e0c87b38252dc171d7ff5bdb129e69ea7
DGxy24 <dolyboantua@gmail.com>
2024-08-21 13:47:16 +0700

    fitur

--- resources/views/Dashboard/Status/index.blade.php
+++ resources/views/Dashboard/Status/index.blade.php
@@ -50,40 +50,48 @@
 
     <div class="card mb-3">
         <div class="card-header">
-            <strong>Tiket ID: tiket_id</strong>
+            <strong>Tiket ID: {{ $tiket[0]->tiket_id }}</strong>
         </div>
         <div class="card-body">
             <!-- Chat antara user dan admin -->
+            @foreach($tiket as $item)
+
+            <script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    var pdfModal = document.getElementById("pdfModal");
+                    pdfModal.addEventListener("show.bs.modal", function(event) {
+                        var button = event.relatedTarget;
+                        var pdfSrc =
+                        '{{ asset('storage/' . $item->bukti) }}'; // Gantilah dengan path dinamis jika ada
+                        var pdfFrame = document.getElementById("pdfFrame");
+                        var downloadPdfBtn = document.getElementById("downloadPdfBtn");
+        
+                        // Set src untuk iframe dan href untuk tombol download
+                        pdfFrame.src = pdfSrc;
+                        downloadPdfBtn.href = pdfSrc;
+                    });
+        
+                    pdfModal.addEventListener("hidden.bs.modal", function() {
+                        var pdfFrame = document.getElementById("pdfFrame");
+                        pdfFrame.src = ""; // Kosongkan src saat modal ditutup
+                    });
+                });
+            </script>
             <div class="media mb-3 d-flex align-items-center">
                 <img src="/img/profile-user.png" class="rounded-circle" alt="User Avatar" width="40" height="40">
                 <div class="media-body ml-3">
-                    <h5 class="mt-0">nama_user</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit excepturi exercitationem mollitia
-                        architecto vitae praesentium similique delectus beatae ad doloribus error, libero quia eius, hic
-                        asperiores illum, suscipit veniam deserunt!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, accusamus laborum cumque omnis error
-                        explicabo quasi ipsum nesciunt esse, repellat inventore consequuntur velit magni ex eos
-                        necessitatibus in earum animi.</p>
-                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#pdfModal">
+                    <h5 class="mt-0">{{ $item->user->name }}</h5>
+                    <p>{{ $item->tindakan }}</p>
+                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#pdfModal"
+                    onclick="ShowPDF( '{{ asset('storage/' . $item->bukti) }}');">
                         <i class="bi bi-filetype-pdf"></i>
                     </button>
                 </div>
+ 
             </div>
-            <div class="media mb-3 d-flex align-items-center">
-                <img src="/img/profile-user.png" class="rounded-circle" alt="Admin Avatar" width="40" height="40">
-                <div class="media-body ml-3">
-                    <h5 class="mt-0">nama_admin</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit excepturi exercitationem mollitia
-                        architecto vitae praesentium similique delectus beatae ad doloribus error, libero quia eius, hic
-                        asperiores illum, suscipit veniam deserunt!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, accusamus laborum cumque omnis error
-                        explicabo quasi ipsum nesciunt esse, repellat inventore consequuntur velit magni ex eos
-                        necessitatibus in earum animi.</p>
-                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#pdfModal">
-                        <i class="bi bi-filetype-pdf"></i>
-                    </button>
-                </div>
-            </div>
+            <hr>
+            @endforeach
+            
         </div>
         <div class="card-footer">
             <a class="btn btn-danger btn-sm"><span data-feather="eye"><i class="bi bi-envelope"></i></span> Kirim Pesan</a>
@@ -92,25 +100,58 @@
 
 
     <!-- Modal -->
-    <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="pdfModalLabel" aria-hidden="true">
+    {{-- <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="pdfModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="pdfModalLabel">Bukti PDF</h5>
                     <div class="d-flex">
+
                         <a id="downloadPdfBtn" href="#" class="btn btn-success btn-sm me-2" download>
                             Download PDF
                         </a>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                 </div>
+                
                 <div class="modal-body">
-                    <iframe src="" id="pdfFrame" width="100%" height="500px"></iframe>
+                    <iframe src="{{ asset('storage/' . $item->bukti)}}" id="pdfFrame" width="100%" height="500px"></iframe>
                 </div>
             </div>
         </div>
+    </div> --}}
+    <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    
+                    <h5 class="modal-title" id="exampleModalLabel">Bukti</h5>
+                    
+                    <div class="d-flex">
+
+                        {{-- <a id="downloadPdfBtn" href="#" class="btn btn-success btn-sm me-2" download>
+                            Download PDF
+                        </a> --}}
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                </div>
+                
+                <div class="modal-body">
+                    <table class="table table-sm">
+                        <tr>
+                            <th>File PDF</th>
+                            <td>
+                                <iframe id="modalPdf" src="" width="100%" height="400px"></iframe>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+              
+            </div>
+        </div>
     </div>
+
+
+    
 @endsection
-@section('scripts')
-    <script src="{{ asset('js/modal.js') }}"></script>
-@endsection
+
